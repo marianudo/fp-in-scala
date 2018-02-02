2901406492bab336327b126a24caa33873bbcb45
Mariano Navas <mariano.navas@clluc.com>
2018-02-02 21:04:13 +0100

    Add init
    Signed-off-by: Mariano Navas <mariano.navas@clluc.com>

--- src/main/scala/chapters/ch03/List.scala
+++ src/main/scala/chapters/ch03/List.scala
@@ -51,4 +51,10 @@ object List {
     case Nil => a2
     case Cons(h, t) => Cons(h, append(t, a2))
   }
+
+  def init[A](l: List[A]): List[A] = l match {
+    case Nil => Nil
+    case Cons(_, Nil) => Nil
+    case Cons(h, t) => Cons(h, init(t))
+  }
 }
