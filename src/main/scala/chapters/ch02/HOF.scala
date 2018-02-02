ec9f3c423cc2e48ad9db4cb43cbd376362bdae99
Mariano Navas <mariano.navas@clluc.com>
2018-02-02 19:44:35 +0100

    Add uncurry
    Signed-off-by: Mariano Navas <mariano.navas@clluc.com>

--- src/main/scala/chapters/ch02/HOF.scala
+++ src/main/scala/chapters/ch02/HOF.scala
@@ -34,4 +34,7 @@ object HOF {
   def partial1[A,B,C](a: A, f: (A,B) => C): B => C = b => f(a, b)
 
   def curry[A,B,C](f: (A, B) => C): A => (B => C) = a => b => f(a, b)
+
+  def uncurry[A,B,C](f: A => B => C): (A, B) => C = (a, b) => f(a)(b)
+
 }
