32e55889626c2e17ae834d76397c9a13be5b4f3e
Mariano Navas <mariano.navas@clluc.com>
2018-02-28 17:35:56 +0100

    Basic functions on either
    Signed-off-by: Mariano Navas <mariano.navas@clluc.com>

--- src/main/scala/chapters/ch04/EitherExercises.scala
+++ src/main/scala/chapters/ch04/EitherExercises.scala
@@ -1,5 +1,33 @@
 package chapters.ch04
 
+import scala.{Either => _}
+
 object EitherExercises {
 
+  case class Left[+E](value: E) extends Either[E, Nothing]
+  case class Right[+A](value: A) extends Either[Nothing, A]
+
+  trait Either[+E, +A] {
+    def map[B](f: A => B): Either[E, B] = this match {
+      case Left(e) => Left(e)
+      case Right(r) => Right(f(r))
+    }
+
+    def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = this match {
+      case Left(l) => Left(l)
+      case Right(r) => f(r)
+    }
+
+
+    def orElse[EE >: E,B >: A](b: => Either[EE, B]): Either[EE, B] = this match {
+      case Left(_) => b
+      case Right(r) => Right(r)
+    }
+
+    def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] =
+      for {
+        aa <- this
+        bb <- b
+      } yield f(aa, bb)
+  }
 }
