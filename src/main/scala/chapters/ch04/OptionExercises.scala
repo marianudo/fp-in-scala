432df494c1fa5a2c59259e3425f1dd19c25293ef
Mariano Navas <mariano.navas@clluc.com>
2018-02-28 15:41:43 +0100

    Variation on some functions
    Signed-off-by: Mariano Navas <mariano.navas@clluc.com>

--- src/main/scala/chapters/ch04/OptionExercises.scala
+++ src/main/scala/chapters/ch04/OptionExercises.scala
@@ -8,10 +8,19 @@ object OptionExercises {
     def map[B](f: A => B): Option[B] =
       flatMap(a => Some(f(a)))
 
+    def map_2[B](f: A => B): Option[B] = this match {
+      case Some(a) => Some(f(a))
+      case None => None
+    }
+
+
     def flatMap[B](f: A => Option[B]): Option[B] = this match {
       case Some(a) => f(a)
       case _ => None
     }
+
+    def flatMap_2[B](f: A => Option[B]): Option[B] =
+      map(f) getOrElse None
 
     def getOrElse[B >: A](default: => B): B = this match {
       case Some(b) => b
@@ -26,6 +35,9 @@ object OptionExercises {
     def filter(f: A => Boolean): Option[A] =
       if(map(f).getOrElse(false)) this
       else None
+
+    def filter_1(f: A => Boolean): Option[A] =
+      flatMap(a => if(f(a)) Some(a) else None)
   }
 
   case class Some[A](a: A) extends Option[A]
