c7d4a15b8efd51e6178fd19a26f4de29b49d8a5a
Mariano Navas <mariano.navas@clluc.com>
2018-02-28 16:37:00 +0100

    Implements sequence & traverse
    Signed-off-by: Mariano Navas <mariano.navas@clluc.com>

--- src/main/scala/chapters/ch04/OptionAndEitherExercises.scala
+++ src/main/scala/chapters/ch04/OptionAndEitherExercises.scala
@@ -51,4 +51,14 @@ object OptionAndEitherExercises {
     def map2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] =
       a.flatMap(aa => b.map(bb => f(aa, bb)))
   }
+
+  def sequence[A](a: List[Option[A]]): Option[List[A]] =
+    a match {
+      case Nil => Some(Nil)
+      case h :: t => h flatMap (hh => sequence(t) map (hh :: _))
+    }
+
+  def traverse[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] =
+    sequence(a.map(f))
+
 }
